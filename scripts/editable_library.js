ce29f12eec1e6ecf2826215b9a3cbedc88377bc7
vladislav.kolodka <vladislav.kolodka@apriorit.com>
2019-03-11 17:57:15 +0200

    beta

--- scripts/editable_library.js
+++ scripts/editable_library.js
@@ -1,25 +1,39 @@
-var storage = `
-    {
-        "books": [
-            {"id": 1, "name": "Book 1", "releaseDate": "1990-01-01", "raiting": 5, "pages": 10},
-            {"id": 2, "name": "Book 2", "releaseDate": "1992-01-01", "raiting": 9, "pages": 50}
-        ],
-        
-        "authors": [
-            {"id": 1, "firstName": "John", "lastName": "Smith"},
-            {"id": 2, "firstName": "Martin", "lastName": "Lloyd"}
-        ]
-    }
-`;
 
 
 (function () {
+
+    function Storage(){
+        this.initData = `
+            {
+                "books": [
+                    {"id": 1, "name": "Book 1", "releaseDate": "1990-01-01", "raiting": 5, "pages": 10, "authors": [ 1 ]},
+                    {"id": 2, "name": "Book 2", "releaseDate": "1992-01-01", "raiting": 9, "pages": 50, "authors": [ 2 ]}
+                ],
+                
+                "authors": [
+                    {"id": 1, "firstName": "John", "lastName": "Smith"},
+                    {"id": 2, "firstName": "Martin", "lastName": "Lloyd"}
+                ]
+            }
+        `;
+    }
+
+    Storage.prototype.load = function(){
+        return JSON.parse(localStorage.getItem('data') || this.initData);
+    }
+
+    Storage.prototype.save = function(data){
+        return localStorage.setItem('data', data);
+    }
+    
     function BookModel(data) {
         var modelMapping = {
             copy: ['id']
         };
 
         ko.mapping.fromJS(data, modelMapping, this);
+
+        this.dirtyFlag = new ko.dirtyFlag(this, false);
     }
 
     function AuthorModel(data) {
@@ -34,7 +48,15 @@ var storage = `
         ko.mapping.fromJS(data, modelMapping, this);
     }
 
-    function PageViewModel(data) {
+    AuthorModel.prototype.toJSON = function() {
+        var copy = ko.toJS(this); //easy way to get a clean copy
+        delete copy.name; //remove an extra property
+        return copy; //return the copy to be serialized
+    }
+
+    function PageViewModel(data, saveCallback) {
+        var self = this;
+
         var modelMapping = {
             books: {
                 key: function (obj) {
@@ -54,11 +76,27 @@ var storage = `
             }
         };
 
+        this.save = function(book) {           
+            var data = ko.toJSON(self, function(key, value){
+                if(key === '__ko_mapping__'){
+                    return undefined;
+                }
+
+                return value;
+            });
+
+            saveCallback(data);
+
+            book.dirtyFlag.reset();
+        }
+
         ko.mapping.fromJS(data, modelMapping, this);
     }
 
     function Main() {
-        ko.applyBindings(new PageViewModel(JSON.parse(storage)))
+        var storage = new Storage();
+
+        ko.applyBindings(new PageViewModel(storage.load(), storage.save))
     }
 
     document.addEventListener("DOMContentLoaded", Main, false);
